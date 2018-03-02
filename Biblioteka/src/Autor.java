d3005e7b97983aef57a6245fa1c91a5671ce5743
zukanovicmilica <zukanovic.milica@gmail.com>
2018-03-02 11:16:03 +0100

    Refaktorisana klasa Knjiga, uvedena klasa Autor

---
+++ Biblioteka/src/Autor.java
@@ -0,0 +1,8 @@
+
+public class Autor {
+	
+	private String ime,prezime;
+	
+	
+
+}
--- Biblioteka/src/Knjiga.java
+++ Biblioteka/src/Knjiga.java
@@ -1,9 +1,50 @@
 
 public class Knjiga {
 	
-	private String naslov, isbn, izdavac, autori;
+	private String naslov, isbn, izdavac;
 	private int izdanje;
+	private Autor[] autori;
 	
+	
+	public String getNaslov() {
+		return naslov;
+	}
+	
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String getIzdavac() {
+		return izdavac;
+	}
+	
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+
+	public int getIzdanje() {
+		return izdanje;
+	}
+	
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+
+	public Autor[] getAutori() {
+		return autori;
+	}
+
+	public void setAutori(Autor[] autori) {
+		this.autori = autori;
+	}
 	
 
 }
