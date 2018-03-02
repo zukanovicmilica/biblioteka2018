17cb5a719c74babf29f4f6df273a6312ff5890fe
zukanovicmilica <zukanovic.milica@gmail.com>
2018-03-02 11:37:17 +0100

    Dodat interfejs

--- Biblioteka/src/biblioteka/interfejs/BibilotekaInterfejs.java
+++ Biblioteka/src/biblioteka/interfejs/BibilotekaInterfejs.java
@@ -1,11 +1,16 @@
 package biblioteka.interfejs;
 
+import java.util.LinkedList;
+
+import biblioteka.Autor;
 import biblioteka.Knjiga;
 
 public interface BibilotekaInterfejs {
 	
 	public void dodajKnjigu(Knjiga k);
 	public void obrisiKnjigu(Knjiga k);
+	public LinkedList<Knjiga> vratiSveKnjige();
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac);
 	
 
 }
