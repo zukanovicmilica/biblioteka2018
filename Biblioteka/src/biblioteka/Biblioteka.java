ec5e14dacd58f0594b26225e354c4ef8ca064725
zukanovicmilica <zukanovic.milica@gmail.com>
2018-03-09 14:45:49 +0100

    Uvedena logicka kontrola u klasu Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -10,12 +10,16 @@ public class Biblioteka implements BibilotekaInterfejs {
 
 	@Override
 	public void dodajKnjigu(Knjiga k) {
+		if (k == null || knjige.contains(k))
+			throw new RuntimeException("Knjiga je null ili vec postoji.");
 		knjige.add(k);
 
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga k) {
+		if (k == null || knjige.contains(k))
+			throw new RuntimeException("Knjiga je null ili vec postoji.");
 		knjige.remove(k);
 
 	}
@@ -28,6 +32,9 @@ public class Biblioteka implements BibilotekaInterfejs {
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
 		LinkedList<Knjiga> novaLista = new LinkedList<Knjiga>();
+
+		if (naslov == null)
+			throw new RuntimeException("Morate uneti naslov.");
 
 		for (int i = 0; i < knjige.size(); i++) {
 			if (knjige.get(i).getNaslov().contains(naslov))
